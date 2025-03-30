04454137e3e8720acf46b6a04dd4cb67eeda835d
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2025-03-30 19:53:41 +0600

    Changed model providors

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -2,37 +2,38 @@ import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-} from 'ai';
-import { groq } from '@ai-sdk/groq';
-import { xai } from '@ai-sdk/xai';
-import { isTestEnvironment } from '../constants';
+} from "ai";
+import { groq } from "@ai-sdk/groq";
+import { xai } from "@ai-sdk/xai";
+import { google } from "@ai-sdk/google";
+import { isTestEnvironment } from "../constants";
 import {
   artifactModel,
   chatModel,
   reasoningModel,
   titleModel,
-} from './models.test';
+} from "./models.test";
 
 export const myProvider = isTestEnvironment
   ? customProvider({
       languageModels: {
-        'chat-model': chatModel,
-        'chat-model-reasoning': reasoningModel,
-        'title-model': titleModel,
-        'artifact-model': artifactModel,
+        "chat-model": chatModel,
+        "chat-model-reasoning": reasoningModel,
+        "title-model": titleModel,
+        "artifact-model": artifactModel,
       },
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-1212'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: groq('deepseek-r1-distill-llama-70b'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        "chat-model": google("gemini-2.0-flash-001"),
+        "chat-model-reasoning": wrapLanguageModel({
+          model: groq("deepseek-r1-distill-llama-70b"),
+          middleware: extractReasoningMiddleware({ tagName: "think" }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        "title-model": xai("grok-2-1212"),
+        "artifact-model": xai("grok-2-1212"),
       },
       imageModels: {
-        'small-model': xai.image('grok-2-image'),
+        "small-model": xai.image("grok-2-image"),
       },
     });
